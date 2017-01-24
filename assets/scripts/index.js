41c4c2686bb0fb9f302c3b1b2a686554be34a811
ashlenprice <ashlen.price@gmail.com>
2017-01-24 11:44:13 -0500

    Test Deployment workflow

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,6 +5,13 @@ const config = require('./config');
 
 $(() => {
   setAPIOrigin(location, config);
+
+  // when form is submitted then console log the input
+  $("form").on("submit", function(event){
+    event.preventDefault();
+    let input = $("#name").val();
+    console.log(input);
+  });
 });
 
 // use require with a reference to bundle the file and use it in this file
